4b8f58699c31a2f50f131372fcbef17616ecf9f6
Alan <alannnc@gmail.com>
2024-02-13 18:57:22 -0700

    cleanup main index file

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -16,75 +16,6 @@ import bookingRoute from "./routes/booking";
 import reviewRoute from "./routes/review";
 // import paymentRoute from "./routes/payment";
 
-// const userSchema = z.object({
-//   id: z.string(),
-//   name: z.string(),
-//   email: z.string().email(),
-// });
-
-// const businessSchema = z.object({
-//   id: z.string(),
-//   name: z.string(),
-//   legalName: z.string(),
-//   description: z.string(),
-//   images: z.array(z.string()),
-//   location: z.string(),
-//   type: z.string(),
-//   owner: z.string(),
-//   lat: z.number(),
-//   long: z.number(),
-// });
-
-// const findServiceSchema = z.object({
-//   country: z.string().optional(),
-//   state: z.string().optional(),
-//   city: z.string().optional(),
-// });
-
-// const serviceSchema = z.object({
-//   id: z.string(),
-//   name: z.string(),
-//   description: z.string(),
-//   images: z.array(z.string()),
-//   price: z.number(),
-//   unit: z.string(),
-//   location: z.string(),
-//   type: z.string(),
-//   owner: z.string(),
-//   status: z.string(),
-//   lat: z.number(),
-//   long: z.number(),
-//   country: z.string(),
-//   state: z.string(),
-//   city: z.string(),
-// });
-
-// const bookingSchema = z.object({
-//   id: z.string(),
-//   serviceId: z.string(),
-//   userId: z.string(),
-//   status: z.string(),
-//   start_date: z.string(),
-//   end_date: z.string(),
-// });
-
-// const reviewSchema = z.object({
-//   id: z.string(),
-//   bookingId: z.string(),
-//   serviceId: z.string(),
-//   userId: z.string(),
-//   userName: z.string(),
-//   rating: z.number(),
-//   comment: z.string(),
-// });
-
-// const paymentSchema = z.object({
-//   id: z.string(),
-//   booking: z.string(),
-//   amount: z.number(),
-//   status: z.string(),
-// });
-
 const globalStateSchema = z.object({
   users: z.array(userSchema),
   business: z.array(businessSchema),
@@ -190,116 +121,9 @@ export default Server(() => {
     Principal.fromText(process.env.CKBTC_MINTER_CANISTER_ID!)
   );
 
-  // const router = express.Router();
-
   app.get("/global-state", (req, res) => {
     res.json(globalState);
   });
-
-  // api endpoints
-
-  // sign in using II @@NOTE: not working for express right now
-
-  // user profile creation
-  // name, email
-  // app.post("/create-user", (req, res) => {
-  //   const data = userSchema.parse(req.body);
-
-  //   globalState.users.push(data);
-
-  //   res.json({ success: true });
-  // });
-
-  // business creation
-  // name, address, legal name, phone, email, website, socials, description, images
-  // app.post("/create-business", (req, res) => {
-  //   const data = serviceSchema.parse(req.body);
-
-  //   globalState.services.push(data);
-
-  //   res.json({ success: true });
-  // });
-
-  // create service
-  // name, description, images, price, location, type, owner
-  // app.post("/create-service", (req, res) => {
-  //   const data = serviceSchema.parse(req.body);
-
-  //   globalState.services.push(data);
-
-  //   res.json({ success: true });
-  // });
-
-  // list services by location
-  // app.get("/list-services", (req, res) => {
-  //   const data = findServiceSchema.parse(req.query);
-  //   const { country, state, city } = data;
-
-  //   // @TODO: list by location
-  //   // const services = globalState.services.filter(service => {
-  //   //   const serviceLocation = `${service.country}, ${service.state}, ${service.city}`;
-  //   //   return serviceLocation === `${country}, ${state}, ${city}`;
-  //   // });
-
-  //   const services = globalState.services;
-
-  //   res.json(services);
-  // });
-
-  // find services by id
-  // app.get("/find-service", (req, res) => {
-  //   const findServiceSchema = z.object({
-  //     id: z.string(),
-  //   });
-  //   const data = findServiceSchema.parse(req.query);
-  //   const { id } = data;
-
-  //   const services = globalState.services.find((service) => {
-  //     return service.id === id;
-  //   });
-
-  //   res.json(services);
-  // });
-
-  // app.get("/find-reviews", (req, res) => {
-  //   const findReviewsByServiceIdSchema = z.object({
-  //     id: z.string(),
-  //   });
-  //   const data = findReviewsByServiceIdSchema.parse(req.query);
-  //   const { id } = data;
-
-  //   const reviews = globalState.reviews.filter((review) => {
-  //     return review.serviceId === id;
-  //   });
-
-  //   res.json(reviews);
-  // });
-
-  // book service
-  // app.post("/book-service", (req, res) => {
-  //   const data = bookingSchema.parse(req.body);
-
-  //   globalState.bookings.push(data);
-
-  //   res.json({ success: true });
-  // });
-
-  // accept/cancel booking
-  // app.put("/update-booking", (req, res) => {
-  //   const data = bookingSchema.parse(req.body);
-
-  //   const { id, status } = data;
-
-  //   const booking = globalState.bookings.find((booking) => {
-  //     return booking.id === id;
-  //   });
-
-  //   if (booking) {
-  //     booking.status = status;
-  //   }
-
-  //   res.json({ success: true });
-  // });
 
   app.get("/balance", async (req, res) => {
     try {
@@ -310,12 +134,6 @@ export default Server(() => {
       throw error;
     }
   });
-
-  // pay booking
-
-  // review service
-
-  // reply service
 
   app.use("/auth", authRoute);
 
